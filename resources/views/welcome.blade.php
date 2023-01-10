80f04ac42f8503f6ccb3d1cfcf8cde5f180199e2
Ewen35550 <ewen.herve.35550@gmail.com>
2023-01-10 11:36:04 +0100

    Ajout du prénom dans le titre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                     </svg>
                 </div>
 
-                <h1>Project Laravel - MDS</h1>
+                <h1>Project Laravel Ewen - MDS</h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
