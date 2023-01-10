f1547f1459c313a7b05728ff1f8641f54f00eba4
Ewen35550 <ewen.herve.35550@gmail.com>
2023-01-10 14:06:28 +0100

    Affiche la nom de la personne connectée

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,9 @@
                     </svg>
                 </div>
 
-                <h1>Project Laravel Ewen - MDS</h1>
+                @auth
+                <h1>{{ Auth::user()->name }}</h1>
+                @endauth
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
